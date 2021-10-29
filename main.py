5a02afe4afb4570df41c67820770c93bb894a835
humzahmalik07 <89996008+humzahmalik07@users.noreply.github.com>
2021-10-29 03:37:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+class MapTile:
+    """ This Map uses x and y coordinates to print out location"""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def location(self):
+        print("your current location is ", self.x, self.y)
+
+
+def start():
+    class firstTile(MapTile):
+        print("You are the start. enter direction to go")
+        Tile = MapTile(0, 1)
+        Tile.location()
+        direction = input("Where do you want to go: ")
+        if direction == "forward":
+            second()
+
+
+def second():
+    class firstTile(MapTile):
+        print("You are at the second tile. enter direction to go")
+        Tile_2 = MapTile(0, 2)
+        Tile_2.location()
+        direction = input("Where do you want to go: ")
+        if direction == "backward":
+            start()
+            
+
+a1 = start()
+a2 = second()
+
+a1
+a2
+
+gamemap = {a1, a2,
+          } 
